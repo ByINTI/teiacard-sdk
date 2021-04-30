2cc9d4ce3dd9d4d0259e35645e9685630500068d
RillianDiello <rilliansf@gmail.com>
2021-04-30 10:54:54 -0400

    # Implementação do EndPoint de Empresas

--- examples/5-returns.php
+++ examples/5-returns.php
@@ -10,55 +10,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once './functions/ReturnsCli.php';
 
 echo "############################################################################################";
-$redis = new Redis() or die("Cannot load Redis module.");
-$redis->connect('redis');
 
 try {
-
-    $token        = $redis->get('teia-card-access-token') ?: null;
-    $refreshToken = $redis->get('teia-card-refresh-token') ?: null;
-
-    $teiaCard = new Client(false, $token);
-
-    if (!$token) {
-        echo "\n# Token nao encontrado no Redis\n";
-        echo "\n# Iniciando autenticacao com username/password...\n";
-
-        $credentials = new Credenciais();
-        $credentials->setClientId(getenv('CLIENT_ID'))
-                    ->setClientSecret(getenv('CLIENT_SECRET'));
-
-        if ($refreshToken) {
-            $credentials->setRefreshToken($refreshToken)
-                        ->setGrantTypeRefreshToken();
-        } else {
-            $credentials->setUsername(getenv('USERNAME'))
-                        ->setPassword(getenv('PASSWORD'))
-                        ->setGrantTypePassword();
-        }
-
-        $authResponse = $teiaCard->autenticacao()->login($credentials);
-
-        echo "\n# Resposta da autenticacao:\n\n";
-        echo $authResponse->toJson(JSON_PRETTY_PRINT);
-
-        $token = $authResponse->get('access_token');
-
-        $redis->set(
-            'teia-card-access-token',
-            $token,
-            $authResponse->get('expires_in')
-        );
-
-        $redis->set(
-            'teia-card-refresh-token',
-            $authResponse->get('refresh_token'),
-            $authResponse->get(24 * 60 * 60)
-        );
-
-        echo "\n\n# Token e Refresh Token setados no Redis\n";
-    }
-
     $test = returnTest();
 
     if (count($test) > 2) {
